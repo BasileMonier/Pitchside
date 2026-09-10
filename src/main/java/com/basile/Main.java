09102f3b8e6e836593628fb6bd0e168131e5a95a
Basile Monier <basilemonier@gmail.com>
2026-09-10 16:36:42 +0200

    feat: ajout des setters de statistique et de la fonction overall de la classe joueur

--- src/main/java/com/basile/Main.java
+++ src/main/java/com/basile/Main.java
@@ -10,9 +10,9 @@ Public class Joueur {
         this.nom = nom;
         this.age = age;
         this.poste = poste;
-        this.stat_technique = stat_technique;
-        this.stat_physique = stat_physique;
-        this.stat_mental = stat_mental;
+        setStat_technique(stat_technique);
+        setStat_physique(stat_physique);
+        setStat_mental(stat_mental);
     }
 
     public enum Poste {
@@ -41,5 +41,34 @@ Public class Joueur {
 
     public int getStat_mental() {
         return stat_mental;
+    }
+
+    public void setStat_technique(int stat_technique) {
+        if (stat_technique < 0 || stat_technique > 100) {
+            throw new IllegalArgumentException("La valeur doit être comprise entre 0 et 100.");
+        }
+        this.stat_technique = stat_technique;
+    }
+
+    public void setStat_physique(int stat_physique) {
+        if (stat_physique < 0 || stat_physique> 100) {
+            throw  new IllegalArgumentException("La valeur doit être comprise entre 0 et 100.");
+        }
+        this.stat_physique = stat_physique;
+    }
+
+    public void setStat_mental(int stat_mental) {
+        if (stat_mental < 0 || stat_mental > 100) {
+            throw new IllegalArgumentException("la valeur doit être comprise entre 0 et 100.");
+        }
+        this.stat_mental = stat_mental;
+    }
+
+    public void setPoste(Poste poste) {
+        this.poste = poste;
+    }
+
+    public int overall(){
+        return (stat_technique + stat_physique + stat_mental) / 3;
     }
 }
