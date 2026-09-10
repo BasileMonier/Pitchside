d3c97dc5148f72b25df2a8c9911ccdf63c1e4f5b
Basile Monier <basilemonier@gmail.com>
2026-09-10 13:05:56 +0200

    feat: ajout de classe Joueur

--- src/main/java/com/basile/Main.java
+++ src/main/java/com/basile/Main.java
@@ -1,3 +1,25 @@
-lic enum Poste {
-    GARDIEN, DEFENSEUR, MILIEU, ATTAQUANT
+Public class Joueur {
+    private String nom;
+    private int age;
+    private Poste poste;
+    private int stat_technique;
+    private int stat_physique;
+    private int stat_mental;
+
+    public Joueur(String nom, int age, Poste poste, int stat_technique, int stat_physique, int stat_mental) {
+        this.nom = nom;
+        this.age = age;
+        this.poste = poste;
+        this.stat_technique = stat_technique;
+        this.stat_physique = stat_physique;
+        this.stat_mental = stat_mental;
+    }
+
+    public enum Poste {
+        GARDIEN, DEFENSEUR, MILIEU, ATTAQUANT
+    }
+
+
+
+
 }
