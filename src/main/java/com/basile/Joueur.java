489f4c7cc2975a3ef79a89fb8f441fc7528bcfd7
Basile Monier <basilemonier@gmail.com>
2026-09-11 14:56:10 +0200

    feat: ajout de 2 nouveaux joueurs

--- src/main/java/com/basile/Joueur.java
+++ src/main/java/com/basile/Joueur.java
@@ -77,4 +77,10 @@ public class Joueur {
 void main() {
     Joueur joueur1 = new Joueur("Basile", 22, Joueur.Poste.DEFENSEUR, 72, 81, 80);
     System.out.println(joueur1.overall());
+
+    Joueur joueur2 = new Joueur("Caicedo", 25, Joueur.Poste.MILIEU, 84, 87, 81);
+    System.out.println(joueur2.overall());
+
+    Joueur joueur3 = new Joueur("Estevao", 19, Joueur.Poste.ATTAQUANT, 88, 80, 78 );
+    System.out.println(joueur3.overall());
 }
