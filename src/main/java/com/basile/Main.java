17b115692e0854a060dcb1e95d080262398ed401
Basile Monier <basilemonier@gmail.com>
2026-09-16 12:09:53 +0200

    fix: modif du main

--- src/main/java/com/basile/Main.java
+++ src/main/java/com/basile/Main.java
@@ -1,23 +1,23 @@
 package com.basile;
 
-public class Main() {
+public class Main{
     public static void main (String[] args) {
 
 
-        Joueur.Poste posteChoisi = Joueur.Poste.GARDIEN;
+        Joueur.Poste posteChoisi = Joueur.Poste.MILIEU;
         Joueur joueur1;
 
         if (posteChoisi == Joueur.Poste.GARDIEN) {
-            joueur1 = new Gardien("Basile", 22, Joueur.Poste.GARDIEN, 72, 81);
+            joueur1 = new Gardien("Basile", 22, 80, 72, 81,82, 78 );
             System.out.println(joueur1.overall());
         } else if (posteChoisi == Joueur.Poste.DEFENSEUR) {
-            joueur1 = new Defenseur("Basile", 22, Joueur.Poste.DEFENSEUR, 72, 81, 80);
+            joueur1 = new Defenseur("Basile", 22, 50, 72, 81, 80, 83);
             System.out.println(joueur1.overall());
         } else if (posteChoisi == Joueur.Poste.MILIEU) {
-            joueur1 = new Milieu("Caicedo", 25, Joueur.Poste.MILIEU, 84, 87, 81);
+            joueur1 = new Milieu("Caicedo", 25, 76, 86, 81, 75, 87);
             System.out.println(joueur1.overall());
         } else if (posteChoisi == Joueur.Poste.ATTAQUANT) {
-            joueur1 = new Attaquant("Estevao", 19, technique:15, );
+            joueur1 = new Attaquant("Estevao", 19, 86, 77, 73, 88, 84);
             System.out.println(joueur1.overall());
         }
     }
